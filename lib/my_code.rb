26aa99fd8640fd53401e1d386763d3c5845048e1
Victoria Cushing <victoriaccushing@gmail.com>
2019-07-30 17:29:22 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,20 @@
-# Your Code Here
+def map(source_array)
+  i = 0 
+  new = []
+  while i < source_array.length do 
+    new.push yield source_array[i]
+    i += 1 
+  end
+  return new
+end
+
+map([1,2,3,-9]) do |block|
+  block * -1
+end
+ 
+map(1,2,3,-9) do |n|
+  n * n
+end
+
+# def reduce(array)
+  
